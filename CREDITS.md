e3f0d30b5b9528c333bdc7e6e580a4f52c072004
MJ Rossetti <datacreativellc@gmail.com>
2017-08-22 14:49:05 -0400

    The ABA has not made it easy to scrape this site. The container div's id keeps changing, and JavaScript produces the table contents. Not cool. Definitely the opposite of an API. Like an anti-API. Not helpful.

--- CREDITS.md
+++ CREDITS.md
@@ -1,3 +1,4 @@
 # Credits, Notes, and Reference
 
   + [`requests` Package Overview](https://github.com/prof-rossetti/nyu-info-2335-70-201706/blob/master/notes/programming-languages/python/packages/requests.md)
+  + [`bs4` Package Overview](https://github.com/prof-rossetti/nyu-info-2335-70-201706/blob/master/notes/programming-languages/python/packages/beautifulsoup.md)
--- app/list_schools.py
+++ app/list_schools.py
@@ -1,13 +1,33 @@
 import requests
+from bs4 import BeautifulSoup
 from IPython import embed
 
 schools = []
 
-URL = "https://www.americanbar.org/groups/legal_education/resources/aba_approved_law_schools/official-guide-to-aba-approved-law-schools.html"
-response = requests.get(URL)
-print(type(response))
-print(response.status_code)
-print(response.text)
+def get_document(source="Remote"):
+    document = None
+
+    if source == "Remote":
+        URL = "https://www.americanbar.org/groups/legal_education/resources/aba_approved_law_schools/official-guide-to-aba-approved-law-schools.html"
+        print("REQUESTING REMOTE RESOURCE", URL)
+        response = requests.get(URL)
+        print("RESPONSE", response.status_code, response.encoding, dict(response.headers))
+        document = response.text
+
+    elif source == "Local":
+        file_path = "mocks/official_school_guide.html"
+        print("READING LOCAL FILE", file_path)
+        with open(file_path, "r") as file:
+            document = file.read()
+
+    return document
+
+doc = get_document(source="Local")
+
+#soup = BeautifulSoup(doc, "lxml")
+soup = BeautifulSoup(doc)
+
+container = soup.find(id="main-tab-container") #.find("div") # the id of this div changes with each request, which is not helpful, so we find it from its parent div
 
 embed()
 
