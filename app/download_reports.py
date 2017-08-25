0a5ddc69e15a52cd5ed38b87cbc27bbd8ede7b47
MJ Rossetti <datacreativellc@gmail.com>
2017-08-24 21:15:40 -0400

    Download pdf without asking for user input, however it requires a pause which suggests these actions are async.

--- app/download_reports.py
+++ app/download_reports.py
@@ -7,33 +7,23 @@ import os
 URL = "http://employmentsummary.abaquestionnaire.org/"
 REPORTS_DIR = os.getcwd() + "/reports"
 
+#
+# CONFIGURE WEB DRIVER TO AUTO-DOWNLOAD WITHOUT ASKING FOR USER INPUT
+#
+
 profile = webdriver.FirefoxProfile()
 profile.set_preference('browser.download.folderList', 2) # 0=Desktop, 1=Downloads, 2=custom
 profile.set_preference('browser.download.dir', REPORTS_DIR)
-
-
-mime_types = "application/pdf, application/xpdf, application/octet-stream"
-profile.set_preference("browser.helperApps.neverAsk.saveToDisk", mime_types);
-profile.set_preference("browser.download.manager.showWhenStarting",False);
-profile.set_preference("browser.helperApps.neverAsk.openFile", mime_types);
-profile.set_preference("browser.helperApps.alwaysAsk.force", False);
-profile.set_preference("browser.download.manager.useWindow", False);
-profile.set_preference("browser.download.manager.focusWhenStarting", False);
-profile.set_preference("browser.download.manager.alertOnEXEOpen", False);
-profile.set_preference("browser.download.manager.showAlertOnComplete", False);
-profile.set_preference("browser.download.manager.closeWhenDone", True);
-profile.set_preference("pdfjs.disabled", True);
-
-# NONE OF THIS JUNK WORKS, UNFORTUNATELY:
-#profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/msword,application/csv,text/csv,application/rtf,application/xml,text/xml,application/octet-stream,application/vnd.ms-excel,application/zip,text/txt,text/plain,application/pdf,application/x-pdf");
-#profile.set_preference('browser.helperApps.neverAsk.saveToDisk', "application/msword, application/csv, text/csv, image/png, image/jpeg, application/pdf, text/html, text/plain, application/octet-stream")
-#profile.set_preference('browser.download.manager.showWhenStarting', False)
-#profile.set_preference('browser.helperApps.alwaysAsk.force', False);
-#profile.set_preference('browser.helperApps.neverAsk.openFile', 'application/octet-stream')
-#profile.set_preference('browser.download.manager.showAlertOnComplete', False);
-#profile.set_preference('browser.download.manager.closeWhenDone', False);
-
-
+profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/pdf") # this setting is required!
+###profile.set_preference("browser.download.manager.showWhenStarting",False)
+###profile.set_preference("browser.helperApps.neverAsk.openFile", mime_types)
+###profile.set_preference("browser.helperApps.alwaysAsk.force", False)
+###profile.set_preference("browser.download.manager.useWindow", False)
+###profile.set_preference("browser.download.manager.focusWhenStarting", False)
+###profile.set_preference("browser.download.manager.alertOnEXEOpen", False)
+###profile.set_preference("browser.download.manager.showAlertOnComplete", False)
+###profile.set_preference("browser.download.manager.closeWhenDone", True)
+profile.set_preference("pdfjs.disabled", True) # this setting is required!
 
 driver = webdriver.Firefox(firefox_profile=profile)
 driver.get(URL)
@@ -45,7 +35,7 @@ driver.get(URL)
 submission_button = driver.find_element_by_id("btnSubmit")
 
 #
-# YEAR SELECTOR
+# YEAR
 #
 
 year_selector = driver.find_element_by_id("ddlYear")
@@ -54,26 +44,27 @@ year_opt = year_options[0] #> 2016
 year_opt.click()
 
 #
-# SCHOOL SELECTOR
+# SCHOOL
 #
 
 school_selector = driver.find_element_by_id("ddlUniversity")
 school_options = school_selector.find_elements_by_tag_name("option") #> 207
 school_options = [opt for opt in school_options if opt.text != "Select School"] #> 206
 
-for i, opt in enumerate(school_options[0:5]):
-    school = {"name": opt.text, "uuid": opt.get_attribute("value")}
-    print(school)
+#for i, opt in enumerate(school_options[0:5]):
+#    school = {"name": opt.text, "uuid": opt.get_attribute("value")}
+#    print(school)
+#
+#    opt.click()
+#
+#    submission_button.click()
+#
+#    #if i == 0: embed()  # allows the user to interact with dialog to click both "always ", then subsequent downloads will happen automatically
 
-    opt.click()
+opt = school_options[-1]
+opt.click()
+submission_button.click()
 
-    submission_button.click()
-
-    #if i == 0: embed()  # allows the user to interact with dialog to click both "always ", then subsequent downloads will happen automatically
-
-
-### CAN ACCESS THE POPUP DIALOG, BUT CAN'T INTERACT WITH IT UNFORTUNATELY:
-###print("CURRENT WINDOW:", driver.current_window_handle, "ALL WINDOWS:", driver.window_handles)
-###driver.switch_to_window("11")
+embed() # note: the download doesn't happen without the pause. this means we have to respect async actions somehow.
 
 driver.close() # important, closes browser window
